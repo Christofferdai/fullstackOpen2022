999758cab6e6a808a0e5412464b869781297d633
chrisdai <christoffer.dai@outlook.com>
2022-09-28 16:43:34 +0200

    2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -5,39 +5,37 @@ const App = () => {
     { name: 'Arto Hellas' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
-const handleChange = (event) => {
+const handleNameChange = (event) => {
   setNewName(event.target.value)
+}
+const handleNumberChange = (event) => {
+  setNewNumber(event.target.value)
 }
 
 const handleSubmit = (event) => {
   event.preventDefault()
-  
   if (persons.find(person => person.name === newName) ) {
     alert(`${newName} is already added to phonebook`)
     return
   }
-    
-  
-  const newPerson = {name: newName}
-
+  const newPerson = {name: newName, number: newNumber}
   setPersons(persons.concat(newPerson))
   setNewName('')
+  setNewNumber('')
 }
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
-        <div>
-          name: <input value={newName} onChange={handleChange}/>
-        </div>
-        <div>
-          <button type="submit" onClick={handleSubmit}>add</button>
-        </div>
+      <form onSubmit={handleSubmit}>
+        <div>name: <input value={newName} onChange={handleNameChange}/></div>
+        <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
+        <div><button type="submit">add</button></div>
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person => <li key={person.name}>{person.name}</li>)}
+        {persons.map(person => <li key={person.name}>{person.name} {person.number}</li>)}
       </ul>
     </div>
   )
