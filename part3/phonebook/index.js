4051f2480227a11734e54e8581c3e525aa672f13
Chris Dai <christoffer.dai@outlook.com>
2022-10-10 15:17:18 +0200

    3.6

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -31,9 +31,50 @@ app.get('/api/persons', (request, response) => {
 
 app.get('/info', (request, response) => {
     response.send(`<p>Phonebook has info for ${persons.length} people</p> <p>${new Date()}</p>`)
+    
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(p => p.id === id)
+    if(person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    console.log(body);
+    if (!body.name) {
+        return response.status(400).json({
+            error: "name is missing"
+        })
+    }
+    if (!body.number) {
+        return response.status(400).json({
+            error: "number is missing"
+        })
+    }
+    if (persons.find(p => p.name === body.name)) {
+        return response.status(400).json({
+            error: "name must be unique"
+        })
+    }
+    const id = Math.floor(Math.random()*100000)
+    const person = body
+    person.id = id
+    persons = persons.concat(person)
+    response.json(person)
 
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(p => p.id !== id)
+    response.status(204).end()
+})
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
