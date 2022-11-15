79d88e5803211041cc9bec28eb85b2b753c24911
Chris Dai <christoffer.dai@outlook.com>
2022-11-15 11:52:22 +0100

    4.10

--- part4/bloglist/tests/list_api.test.js
+++ part4/bloglist/tests/list_api.test.js
@@ -12,6 +12,7 @@ beforeEach(async () => {
   const promiseArray = blogObjects.map((blog) => blog.save())
   await Promise.all(promiseArray)
 })
+
 test('all blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
@@ -24,6 +25,28 @@ test('all blogs are returned as json', async () => {
 test('the unique identifier property is named id', async () => {
   const blogs = await blogsInDb()
   blogs.forEach((blog) => expect(blog.id).toBeDefined())
+})
+
+test('a blog can be added', async () => {
+  const newBlog = {
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+  }
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await blogsInDb()
+  const titles = blogsAtEnd.map((n) => n.title)
+
+  expect(blogsAtEnd).toHaveLength(initalBlogs.length + 1)
+  expect(titles).toContain(
+    'Go To Statement Considered Harmful',
+  )
 })
 
 afterAll(() => {
