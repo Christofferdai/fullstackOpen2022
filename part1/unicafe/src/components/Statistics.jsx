837ed7d6c0cdbf169bc29d5fd752656f1def92d7
chengdai <chengdai.dev@gmail.com>
2024-06-09 16:37:11 +0200

    completed unicafe

---
+++ part1/unicafe/src/components/Statistics.jsx
@@ -0,0 +1,34 @@
+import StatisticLine from "./StatisticLine"
+
+const Statistics = ({ good, neutral, bad }) => { 
+  const all = good + neutral + bad
+  const average = (good - bad) / all
+  const positive = (good / all) * 100
+
+  if (all === 0) {
+    return (
+      <div>
+        
+        <p>No feedback given</p>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h2>statistics</h2>
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="all" value={all} />
+          <StatisticLine text="average" value={average} />
+          <StatisticLine text="positive" value={positive + ' %'} />
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export  default Statistics
